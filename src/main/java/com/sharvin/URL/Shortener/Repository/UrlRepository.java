14c2fee9956de18570c2e514ebdc1e1c71df4a6b
sharvin18 <dedhiasharvin@gmail.com>
2021-08-11 16:44:44 +0530

    Setting up repo

---
+++ src/main/java/com/sharvin/URL/Shortener/Repository/UrlRepository.java
@@ -0,0 +1,12 @@
+package com.sharvin.URL.Shortener.Repository;
+
+import com.sharvin.URL.Shortener.Models.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UrlRepository extends JpaRepository<Url, Long> {
+
+    public Url findByShortLink(String shortLink);
+
+}
